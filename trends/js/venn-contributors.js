d9de3feec6a9590f9396f58cf8f284165f96ec66
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-10-26 23:15:16 +0200

    Prepared static files as a demo of an app.

---
+++ trends/js/venn-contributors.js
@@ -0,0 +1,33 @@
+function drawVennContributors(containerSelector, dataPath) {
+
+    var chart = d3.select(containerSelector);
+
+    var vennChart = venn.VennDiagram()
+        .width(+chart.attr("width"))
+        .height(+chart.attr("height"));
+
+    d3.json(dataPath, function (error, data) {
+        if (error) throw error;
+
+        chart.datum(data).call(vennChart);
+
+        //var c10 = d3.schemeCategory10();
+
+        d3.selectAll(containerSelector + " .venn-circle path")
+            .style("fill", function(d) { return d.color; });
+
+        // Add transition on mouse over
+        d3.selectAll(containerSelector + " .venn-circle")
+            .on("mouseover", function(d, i) {
+                var node = d3.select(this).transition();
+                node.select("path").style("fill-opacity", d.sets.length == 1 ? .4 : .1);
+            })
+            .on("mouseout", function(d, i) {
+                var node = d3.select(this).transition();
+                node.select("path").style("fill-opacity", d.sets.length == 1 ? .25 : .0);
+
+            });
+
+    });
+
+}
