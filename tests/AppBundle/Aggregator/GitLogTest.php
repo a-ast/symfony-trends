359fac1d5559d65b5217ab1fee32fac3b030f2e4
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-11-01 13:58:33 +0100

    Implemented setUp method.

--- tests/AppBundle/Aggregator/GitLogTest.php
+++ tests/AppBundle/Aggregator/GitLogTest.php
@@ -6,25 +6,46 @@ namespace Tests\AppBundle\Aggregator;
 
 use AppBundle\Aggregator\GitLog;
 use AppBundle\Entity\Contributor;
+use AppBundle\Repository\ContributionHistoryRepository;
+use AppBundle\Repository\ContributionRepository;
+use AppBundle\Repository\ContributorRepository;
 use PHPUnit_Framework_TestCase;
 use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 
 class GitLogTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var ContributorRepository|ObjectProphecy
+     */
+    private $contributorRepository;
+
+    /**
+     * @var ContributionRepository|ObjectProphecy
+     */
+    private $contributionRepository;
+
+    /**
+     * @var ContributionHistoryRepository|ObjectProphecy
+     */
+    private $contributionLogRepository;
+
+    protected function setUp()
+    {
+        $this->contributorRepository = $this->prophesize('AppBundle\Repository\ContributorRepository');
+        $this->contributionRepository = $this->prophesize('AppBundle\Repository\ContributionRepository');
+        $this->contributionLogRepository = $this->prophesize('AppBundle\Repository\ContributionHistoryRepository');
+    }
+
     public function testAggregateOnlyIteratesContributors()
     {
-        $contributorRepository = $this->prophesize('AppBundle\Repository\ContributorRepository');
-
-        $contributorRepository
+        $this->contributorRepository
             ->findByEmail(Argument::type('string'))
             ->willReturn(new Contributor())
             ->shouldBeCalledTimes(7);
 
-        $contributionRepository = $this->prophesize('AppBundle\Repository\ContributionRepository');
-        $contributionLogRepository = $this->prophesize('AppBundle\Repository\ContributionHistoryRepository');
-
-        $aggregator = new GitLog($contributorRepository->reveal(),
-            $contributionRepository->reveal(), $contributionLogRepository->reveal(), __DIR__.'/fixtures/');
+        $aggregator = new GitLog($this->contributorRepository->reveal(),
+            $this->contributionRepository->reveal(), $this->contributionLogRepository->reveal(), __DIR__.'/fixtures/');
 
         $options = [
             'project_id' => 1,
