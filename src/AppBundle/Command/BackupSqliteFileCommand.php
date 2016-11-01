75e0093d030a8e14cd801bb3784c4d25bfe8278b
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-11-01 08:41:39 +0100

    Implemented a command to create a copy of sqlite file.

---
+++ src/AppBundle/Command/BackupSqliteFileCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+class BackupSqliteFileCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('trends:backup-sqlite-file')
+            ->setDescription('Backup sqlite file')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $path = $this->getContainer()->getParameter('database.path');
+        $now = new \DateTime();
+        $newPath = $path.'.'.$now->format(DATE_W3C);
+
+        $fs = new Filesystem();
+        $fs->copy($path, $newPath);
+
+        $output->writeln(sprintf('<info>File copied to: %s</info>', $newPath));
+    }
+}
