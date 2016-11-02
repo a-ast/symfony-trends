e44cfb24eda9ac26647534f599461e7725cbefba
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-11-02 13:38:56 +0100

    Refactore contributor page aggregator.

--- src/AppBundle/Aggregator/ContributorPage.php
+++ src/AppBundle/Aggregator/ContributorPage.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Aggregator;
 
+use AppBundle\Aggregator\Helper\ContributorExtractor;
 use AppBundle\Repository\ContributorRepository;
 use GuzzleHttp\ClientInterface;
 use Symfony\Component\DomCrawler\Crawler;
@@ -16,17 +17,23 @@ class ContributorPage implements AggregatorInterface
      * @var ContributorRepository
      */
     private $repository;
+    /**
+     * @var ContributorExtractor
+     */
+    private $extractor;
 
     /**
      * Constructor.
      *
      * @param ClientInterface $httpClient
+     * @param ContributorExtractor $extractor
      * @param ContributorRepository $repository
      */
-    public function __construct(ClientInterface $httpClient, ContributorRepository $repository)
+    public function __construct(ClientInterface $httpClient, ContributorExtractor $extractor, ContributorRepository $repository)
     {
         $this->httpClient = $httpClient;
         $this->repository = $repository;
+        $this->extractor = $extractor;
     }
 
     public function aggregate(array $options = [])
@@ -34,102 +41,16 @@ class ContributorPage implements AggregatorInterface
         $url = 'http://symfony.com/contributors/code';
 
         $contributors = $this->getContributors($url);
-        $contributorNamesFromPage = array_column($contributors, 0);
+        //$contributorNamesFromPage = array_column($contributors, 0);
 
-        // Step 1. Find doubles
-        $diff = $this->checkDoubles($contributorNamesFromPage);
-        foreach ($diff['page_db'] as $item) {
-            print(sprintf('%s on page: %d  in db: %d',
-                    $item['name'], $item['on_page'], $item['in_db'])).PHP_EOL;
-        }
-        print PHP_EOL;
-
-
-
-
-        // Step 2. Compare DB and page
-        $contributorNames = $this->repository->getContributorNames();
-
-        $i = 0;
-        foreach ($contributorNames as $contributorId => $contributor) {
-            foreach ($contributor['names'] as $name) {
-                if(false !== $key = $this->findArrayItem($name, $contributorNamesFromPage)) {
-                    continue(2);
-                }
-            }
-
-            //print sprintf('php bin/console tr:da:fix %d -o"%s <%s>" -t""', $contributorId, $contributor['names'][0], $contributor['email']).PHP_EOL;
-            $i++;
-        }
-
-        print sprintf('********* Total: %d', $i);
-
-        // Step 3. Compare page and db
-        $i = 0;
-        foreach ($contributorNamesFromPage as $nameFromPage) {
-
-            $found = false;
-
-            foreach ($contributorNames as $contributorId => $contributor) {
-
-                if(in_array($nameFromPage, $contributor['names'])) {
-                    $found = true;
-                    break;
-                }
-
-            }
-
-            if(false === $found) {
-                //print sprintf('%d. %s', ++$i, $nameFromPage).PHP_EOL;
-            }
-        }
-
-        print $i.PHP_EOL;
-
-//        $missingInDb = array_diff($contributorNamesFromPage, $contributorNames);
-//        $missingInPage = array_diff($contributorNames, $contributorNamesFromPage);
-//
-//        var_dump($missingInDb);
-
+        var_dump($contributors);
     }
 
     public function getContributors($url)
     {
         $responseBody = (string)$this->getPageContents($url);
-        $domCrawler = new Crawler($responseBody, $url);
 
-        $contributors = [];
-
-        // @todo:
-        // 1. take path of item
-        // 2. try to find link and take data from it
-        // 3. if not, get text
-
-        // or try to iterate every element, try to find order
-
-
-        $domCrawler
-            ->filterXPath('//ol[position()>1]/li')
-            //->filterXPath(sprintf('//ol[position*(>1]/li', $blockPositionCondition))
-            ->each(function(Crawler $nodeCrawler) use (&$contributors) {
-
-                $name = '';
-                $sliName = '';
-                $nodeCrawler->filterXPath('li/text()')
-                    ->each(function(Crawler $textNode) use (&$name){
-                        $name .= trim($textNode->text());
-                    });
-
-                if('' === $name) {
-                    $urlNode = $nodeCrawler->filterXPath('li/a');
-
-                    $name = trim($urlNode->text());
-                    $sliName = $this->getTextAfter($urlNode->attr('href'), 'https://connect.sensiolabs.com/profile/');
-
-                }
-
-                $contributors[] = [$name, $sliName];
-            });
+        $contributors = $this->extractor->extract($responseBody);
 
         return $contributors;
     }
@@ -205,5 +126,59 @@ class ContributorPage implements AggregatorInterface
     protected function findArrayItem($item, array &$array)
     {
         return array_search(strtolower($item), array_map('strtolower', $array));
+    }
+
+    /**
+     * @param $contributorNamesFromPage
+     */
+    protected function analyzeNames($contributorNamesFromPage)
+    {
+// Step 1. Find doubles
+        $diff = $this->checkDoubles($contributorNamesFromPage);
+        foreach ($diff['page_db'] as $item) {
+            print(sprintf('%s on page: %d  in db: %d',
+                    $item['name'], $item['on_page'], $item['in_db'])).PHP_EOL;
+        }
+        print PHP_EOL;
+
+
+        // Step 2. Compare DB and page
+        $contributorNames = $this->repository->getContributorNames();
+
+        $i = 0;
+        foreach ($contributorNames as $contributorId => $contributor) {
+            foreach ($contributor['names'] as $name) {
+                if (false !== $key = $this->findArrayItem($name, $contributorNamesFromPage)) {
+                    continue(2);
+                }
+            }
+
+            //print sprintf('php bin/console tr:da:fix %d -o"%s <%s>" -t""', $contributorId, $contributor['names'][0], $contributor['email']).PHP_EOL;
+            $i++;
+        }
+
+        print sprintf('********* Total: %d', $i);
+
+        // Step 3. Compare page and db
+        $i = 0;
+        foreach ($contributorNamesFromPage as $nameFromPage) {
+
+            $found = false;
+
+            foreach ($contributorNames as $contributorId => $contributor) {
+
+                if (in_array($nameFromPage, $contributor['names'])) {
+                    $found = true;
+                    break;
+                }
+
+            }
+
+            if (false === $found) {
+                //print sprintf('%d. %s', ++$i, $nameFromPage).PHP_EOL;
+            }
+        }
+
+        print $i.PHP_EOL;
     }
 }
