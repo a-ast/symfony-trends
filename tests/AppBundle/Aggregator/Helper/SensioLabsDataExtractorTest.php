3df07b9e0fe867a52150de5d9085f6495874b679
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-11-06 19:25:16 +0100

    Improved tests for SensiolabsDataExtractor to cover cases when not all data pieces are present on a page.

--- tests/AppBundle/Aggregator/Helper/SensioLabsDataExtractorTest.php
+++ tests/AppBundle/Aggregator/Helper/SensioLabsDataExtractorTest.php
@@ -9,11 +9,10 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class SensiolabsDataExtractorTest extends PHPUnit_Framework_TestCase
 {
-
     public function testExtract()
     {
         $extractor = new SensiolabsDataExtractor();
-        $html = file_get_contents(__DIR__.'/../fixtures/sensiolabs-profile.html');
+        $html = file_get_contents(__DIR__.'/../fixtures/sensiolabs-profiles/gandalf.html');
 
         $crawler = new Crawler($html, 'http://middle-earth');
 
@@ -24,6 +23,25 @@ class SensiolabsDataExtractorTest extends PHPUnit_Framework_TestCase
             'country' => 'Middle Earth',
             'github_url' => 'https://github.com/gandalf',
             'github_login' => 'gandalf',
+        ];
+
+        $this->assertEquals($expected, $data);
+    }
+
+    public function testExtractIfSomeFieldsAreMissing()
+    {
+        $extractor = new SensiolabsDataExtractor();
+        $html = file_get_contents(__DIR__.'/../fixtures/sensiolabs-profiles/frodo.html');
+
+        $crawler = new Crawler($html, 'http://middle-earth');
+
+        $data = $extractor->extract($crawler);
+
+        $expected = [
+            'city' => 'Shire',
+            'country' => '',
+            'github_url' => '',
+            'github_login' => '',
         ];
 
         $this->assertEquals($expected, $data);
