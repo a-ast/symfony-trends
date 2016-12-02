c89be85f8e0a26de086592ea181baaabf43e8dba
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-12-02 23:37:14 +0100

    Implemented contributor count distribution chart.

--- src/AppBundle/Resources/data-views/postgresql/data-source.sql
+++ src/AppBundle/Resources/data-views/postgresql/data-source.sql
@@ -19,7 +19,6 @@ CREATE VIEW vw_contributions_per_year AS
       sum(cast(c.is_core_member as int)) as core_team_contribution_count
   from contribution cn
   left join contributor c on cn.contributor_id = c.id
-  -- where project_id = 1
   group by to_char(cn.commited_at, 'YYYY'), cn.project_id
   order by date asc;
 
@@ -34,9 +33,9 @@ CREATE VIEW vw_contributor_intersection AS
   FROM (
          SELECT
            contributor_id,
-           STRING_AGG(project_id::text, ','::text) as project_ids,
-           STRING_AGG(project_name, ' and '::text) as project_names,
-           TO_HEX(AVG(HEX_TO_INT(color))::int) AS color
+           string_agg(project_id::text, ','::text) as project_ids,
+           string_agg(project_name, ' and '::text) as project_names,
+           to_hex(avg(hex_to_int(color))::int) AS color
          from (
                 SELECT
                   DISTINCT cn.contributor_id, cn.project_id, p.name as project_name, p.color
@@ -47,3 +46,61 @@ CREATE VIEW vw_contributor_intersection AS
          GROUP BY contributor_id
        ) gc
   GROUP BY project_ids, project_names, color;
+
+
+
+DROP FUNCTION IF EXISTS get_range_bounds(bigint, bigint[][]) CASCADE;
+CREATE OR REPLACE FUNCTION get_range_bounds(value bigint, ranges bigint[][]) RETURNS bigint[] AS $$
+DECLARE
+  r bigint[];
+  range_type text;
+BEGIN
+
+  FOREACH r SLICE 1 IN ARRAY ranges
+  LOOP
+    range_type := '[)';
+    IF r[1] = r[2] THEN
+      range_type := '[]';
+    END IF;
+
+    IF value <@ int8range(r[1], r[2], range_type) THEN
+      RETURN r;
+    END IF;
+  END LOOP;
+  RETURN ARRAY[NULL, NULL];
+
+END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+
+DROP FUNCTION IF EXISTS get_range_bounds_description(bigint[2]) CASCADE;
+CREATE OR REPLACE FUNCTION get_range_bounds_description(bounds bigint[2]) RETURNS text AS $$
+BEGIN
+  IF bounds[1] IS NULL AND bounds[2] IS NULL THEN
+    RETURN '';
+  ELSEIF bounds[1] IS NULL THEN
+      RETURN '<' || bounds[2]::text;
+  ELSEIF bounds[2] IS NULL THEN
+      RETURN bounds[1]::text || '+';
+  ELSEIF bounds[1] = bounds[2] THEN
+      RETURN bounds[1]::text;
+  ELSE
+      RETURN bounds[1]::text || '-' || bounds[2]::text;
+  END IF;
+  RETURN '';
+END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
+
+
+DROP VIEW IF EXISTS vw_commit_count_distribution CASCADE;
+CREATE VIEW vw_commit_count_distribution AS
+  SELECT project_id, get_range_bounds_description(get_range_bounds(contribution_count, ARRAY[[1,1],[2,2],[3,5],[5,10],[10,30],[30,200],[200,null]])) as bounds, count(*) as contributor_count
+  FROM
+    (
+      SELECT project_id, contributor_id, count(*) as contribution_count
+      FROM contribution
+      GROUP BY project_id, contributor_id
+      ORDER BY contribution_count ASC
+    ) cn
+  GROUP BY project_id, bounds
+  ORDER BY contributor_count;
