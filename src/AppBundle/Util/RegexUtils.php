6e9480cb8927c0648f74554d23c1f028a5ecdcd6
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-12-16 23:43:17 +0100

    Implemented RegexUtils.

---
+++ src/AppBundle/Util/RegexUtils.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Util;
+
+class RegexUtils
+{
+    /**
+     * @param $text
+     * @param array $regexes
+     *
+     * @return bool
+     */
+    public static function match($text, array $regexes)
+    {
+        foreach ($regexes as $regex) {
+            $match = preg_match('/'.$regex.'/i', $text);
+            if (false === $match) {
+                throw new \UnexpectedValueException(sprintf('%s is not valid regular expression'));
+            }
+
+            if (0 !== $match) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ tests/AppBundle/Util/RegexUtilsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace AppBundle\Util;
+
+use PHPUnit_Framework_TestCase;
+
+class RegexUtilsTest extends PHPUnit_Framework_TestCase
+{
+    public function testMatch()
+    {
+        $this->assertTrue(RegexUtils::match('Frodo Baggins', ['^Bilbo', '^Frodo', '^Sam']));
+        $this->assertTrue(RegexUtils::match('Frodo Baggins', ['^bilbo', '^frodo', '^sam']));
+        $this->assertTrue(RegexUtils::match('Frodo Baggins', ['(udu)', '(odo)']));
+        $this->assertTrue(RegexUtils::match('Frodo Baggins', ['(UDU)', '(ODO)']));
+        $this->assertFalse(RegexUtils::match('Frodo Baggins', ['(ada)', '(ede)']));
+    }
+}
