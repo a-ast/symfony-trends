41f0040a7cb46f6122100fc87a1e3b3f91ff4533
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-12-21 11:30:56 +0100

    Added the spec for SeriesProvider.

---
+++ spec/AppBundle/Provider/SeriesProviderSpec.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace spec\AppBundle\Provider;
+
+use AppBundle\Provider\SeriesProvider;
+use AppBundle\Repository\DataProvider;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * @mixin SeriesProvider
+ */
+class SeriesProviderSpec extends ObjectBehavior
+{
+    function it_is_initializable(DataProvider $dataProvider)
+    {
+        $this->beConstructedWith($dataProvider);
+
+        $this->shouldHaveType(SeriesProvider::class);
+    }
+
+    function it_returns_series(DataProvider $dataProvider)
+    {
+        $this->beConstructedWith($dataProvider);
+
+        $dataProvider
+            ->getData(Argument::any(), Argument::any())
+            ->willReturn([['id' => 1, 'name' => 'Gandalf', 'age' => 2019], ['id' => 1, 'name' => 'Frodo', 'age' => 33]]);
+
+        $options = [
+            [
+                'data_source' => 'data_view',
+                'criteria' => [],
+                'dimensions' => ['name', 'age'],
+            ]
+        ];
+
+        $this->getSeries($options)->shouldReturn([['data' => [['Gandalf', 2019], ['Frodo', 33]]]]);
+    }
+}
