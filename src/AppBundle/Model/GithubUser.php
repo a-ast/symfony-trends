a52753e3eef19e12e41a9435f585a634beb02391
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-12-21 12:36:56 +0100

    Added GithubUser model.

---
+++ src/AppBundle/Model/GithubUser.php
@@ -0,0 +1,75 @@
+<?php
+
+
+namespace AppBundle\Model;
+
+class GithubUser
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $location;
+
+    /**
+     * @var string
+     */
+    private $country;
+
+    /**
+     * Constructor.
+     * @param string $name
+     * @param string $email
+     * @param string $location
+     * @param string $country
+     */
+    public function __construct($name, $email, $location, $country)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->location = $location;
+        $this->country = $country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
