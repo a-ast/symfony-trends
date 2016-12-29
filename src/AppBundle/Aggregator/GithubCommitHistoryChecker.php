d63c29bfa1d9310db5ee8c5dca53813dfd5b5f99
Andrey Astakhov <andrey.astakhov@gmail.com>
2016-12-29 17:40:20 +0100

    Added commit checker.

---
+++ src/AppBundle/Aggregator/GithubCommitHistoryChecker.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace AppBundle\Aggregator;
+
+use AppBundle\Builder\ContributorBuilder;
+use AppBundle\Client\GithubApiClient;
+use AppBundle\Entity\Project;
+use AppBundle\Helper\ProgressInterface;
+use AppBundle\Model\GithubCommit;
+use AppBundle\Repository\ContributionRepository;
+use AppBundle\Repository\ProjectRepository;
+
+class GithubCommitHistoryChecker implements AggregatorInterface
+{
+    /**
+     * @var GithubApiClient
+     */
+    private $apiClient;
+
+    /**
+     * @var ContributorBuilder
+     */
+    private $contributorBuilder;
+
+    /**
+     * @var ContributionRepository
+     */
+    private $contributionRepository;
+
+    /**
+     * @var ProjectRepository
+     */
+    private $projectRepository;
+
+    /**
+     * @var array
+     */
+    private $maintenanceCommitPatterns;
+
+    /**
+     * Constructor.
+     *
+     * @param GithubApiClient $apiClient
+     * @param ContributorBuilder $contributorBuilder
+     * @param ProjectRepository $projectRepository
+     * @param ContributionRepository $contributionRepository
+     * @param array $maintenanceCommitPatterns
+     */
+    public function __construct(
+        GithubApiClient $apiClient,
+        ContributorBuilder $contributorBuilder,
+        ProjectRepository $projectRepository,
+        ContributionRepository $contributionRepository,
+        array $maintenanceCommitPatterns)
+    {
+        $this->apiClient = $apiClient;
+        $this->contributorBuilder = $contributorBuilder;
+        $this->contributionRepository = $contributionRepository;
+        $this->projectRepository = $projectRepository;
+        $this->maintenanceCommitPatterns = $maintenanceCommitPatterns;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function aggregate(array $options, ProgressInterface $progress = null)
+    {
+        $projectId = $options['project_id'];
+        /** @var Project $project */
+        $project = $this->projectRepository->find($projectId);
+
+        if (null === $project) {
+            throw new \RuntimeException(sprintf('Project %d not found', $projectId));
+        }
+
+        $projectRepo = $project->getGithubPath();
+
+        $page = 1;
+
+        while ($commits = $this->apiClient->getCommits($projectRepo, null, $page)) {
+
+            foreach ($commits as $commitData) {
+
+                $commit = new GithubCommit($commitData);
+
+                $existingCommit = $this->contributionRepository->findOneBy(['commitHash' => $commit->getHash()]);
+                if (null === $existingCommit) {
+                    print $commit->getDate()->format('Y:m:d H:i') .' '. $commit->getHash() . "\n";
+                }
+            }
+
+            $page++;
+        }
+    }
+}
