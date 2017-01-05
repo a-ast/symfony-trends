c50cf33c84d51b7c08d34e3cbe6080c89ea177c3
Andrey Astakhov <andrey.astakhov@gmail.com>
2017-01-05 10:05:05 +0100

    Implemented getCommits in ClientAdapter.

--- src/AppBundle/Client/Github/ClientAdapterInterface.php
+++ src/AppBundle/Client/Github/ClientAdapterInterface.php
@@ -1,10 +1,20 @@
 <?php
 namespace AppBundle\Client\Github;
 
+use AppBundle\Model\GithubCommit;
 use DateTimeInterface;
+use Iterator;
 
 interface ClientAdapterInterface
 {
+    /**
+     * @param string $repositoryPath
+     * @param DateTimeInterface|null $since
+     *
+     * @return GithubCommit[]|Iterator
+     */
+    public function getCommits($repositoryPath, DateTimeInterface $since = null);
+
     /**
      * @param string $repositoryPath
      * @param DateTimeInterface|null $since
