a16cb369de7142555d6dbbfa9436a6f8fd91a93e
Andrey Astakhov <andrey.astakhov@gmail.com>
2017-01-20 13:25:55 +0100

    Removed unused method.

--- src/AppBundle/Repository/PullRequestRepository.php
+++ src/AppBundle/Repository/PullRequestRepository.php
@@ -2,31 +2,10 @@
 
 namespace AppBundle\Repository;
 
-use DateTimeImmutable;
-
 /**
  * PullRequestRepository.
  */
 class PullRequestRepository extends Repository
 {
-    /**
-     * @param int $projectId
-     *
-     * @return DateTimeImmutable
-     */
-    public function getLastCreatedAtDate($projectId)
-    {
-        $qb = $this->createQueryBuilder('c')
-            ->select('MAX(c.createdAt)')
-            ->where('c.projectId = :id')
-            ->setParameter('id', $projectId);
 
-        $result = $qb->getQuery()->getSingleScalarResult();
-
-        if (null === $result) {
-            $result = '1970-01-01 00:00:00';
-        }
-
-        return new DateTimeImmutable($result);
-    }
 }
