47fb51d0f48d16bd0303b16717de92f847175d51
Andrey Astakhov <andrey.astakhov@gmail.com>
2017-01-20 11:07:30 +0100

    Fixed spec.

--- spec/AppBundle/Aggregator/PullRequestSpec.php
+++ spec/AppBundle/Aggregator/PullRequestSpec.php
@@ -5,6 +5,7 @@ namespace spec\AppBundle\Aggregator;
 use AppBundle\Aggregator\AggregatorInterface;
 use AppBundle\Aggregator\PullRequest;
 use AppBundle\Client\Github\GithubApiInterface;
+use AppBundle\Repository\PullRequestRepository;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -13,9 +14,9 @@ use Prophecy\Argument;
  */
 class PullRequestSpec extends ObjectBehavior
 {
-    function it_is_initializable(GithubApiInterface $githubApi)
+    function it_is_initializable(GithubApiInterface $githubApi, PullRequestRepository $repository)
     {
-        $this->beConstructedWith($githubApi);
+        $this->beConstructedWith($githubApi, $repository);
         $this->shouldHaveType(PullRequest::class);
         $this->shouldImplement(AggregatorInterface::class);
     }
