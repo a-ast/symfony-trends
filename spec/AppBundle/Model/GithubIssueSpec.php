0da65e1ae4f752bf49fc2924734ce6c96e4f3fbf
Andrey Astakhov <andrey.astakhov@gmail.com>
2017-01-20 13:25:27 +0100

    Implemented GithubIssue VO.

---
+++ spec/AppBundle/Model/GithubIssueSpec.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace spec\AppBundle\Model;
+
+use AppBundle\Model\GithubIssue;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * @mixin GithubIssue
+ */
+class GithubIssueSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(GithubIssue::class);
+    }
+
+    function it_can_be_created_from_response_data()
+    {
+        $this->beConstructedThrough('createFromResponseData', [$this->getResponseData()]);
+
+        $this->shouldHaveType(GithubIssue::class);
+    }
+
+    function it_fails_for_pull_request_response_data()
+    {
+        $responseData = array_merge($this->getResponseData(), ['pull_request' => []]);
+        $this->beConstructedThrough('createFromResponseData', [$responseData]);
+        $this->shouldThrow(\RuntimeException::class)->duringInstantiation();
+    }
+
+    private function getResponseData()
+    {
+        return [
+            'id' => 100,
+            'number' => 200,
+            'state' => 'closed',
+            'title' => '[Ring] I need the Ring...',
+            'user' => [
+                'id' => 200,
+            ],
+            'body' => '...but I fear.',
+            'created_at' => '2010-01-01T00:00:00Z',
+            'updated_at' => '2010-01-02T00:00:00Z',
+            'closed_at' => '2010-01-03T00:00:00Z',
+            'labels' => [
+                ['id' => 1, 'name' => 'Bug'],
+                ['id' => 2, 'name' => 'Feature'],
+            ]
+        ];
+    }
+}
---
+++ src/AppBundle/Model/GithubIssue.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace AppBundle\Model;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+use RuntimeException;
+
+class GithubIssue
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var int
+     */
+    private $number;
+
+    /**
+     * @var string
+     */
+    private $state;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var int
+     */
+    private $userId;
+
+    /**
+     * @var string
+     */
+    private $body;
+
+    /**
+     * @var DateTimeInterface
+     */
+    private $createdAt;
+
+    /**
+     * @var DateTimeInterface
+     */
+    private $updatedAt;
+
+    /**
+     * @var DateTimeInterface
+     */
+    private $closedAt;
+
+    /**
+     * @var array
+     */
+    private $labels;
+
+    /**
+     * Constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param array $data
+     * @return GithubIssue
+     */
+    public static function createFromResponseData(array $data)
+    {
+        if (isset($data['pull_request'])) {
+            throw new RuntimeException('Response must not contain pull request data.');
+        }
+
+        $issue = new GithubIssue();
+
+        $issue->id = (int) $data['id'];
+        $issue->number = (int) $data['number'];
+        $issue->state = $data['state'];
+        $issue->title = $data['title'];
+        $issue->userId = (int) $data['user']['id'];
+        $issue->body = isset($data['body']) ? $data['body'] : '';
+
+        $issue->createdAt = new DateTimeImmutable($data['created_at']);
+        $issue->updatedAt = isset($data['updated_at']) ? new DateTimeImmutable($data['updated_at']) : null;
+        $issue->closedAt = isset($data['closed_at']) ? new DateTimeImmutable($data['closed_at']) : null;
+
+        $issue->labels = array_column($data['labels'], 'name');
+
+        return $issue;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumber()
+    {
+        return $this->number;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getClosedAt()
+    {
+        return $this->closedAt;
+    }
+
+    /**
+     * @return array
+     */
+    public function getLabels()
+    {
+        return $this->labels;
+    }
+}
