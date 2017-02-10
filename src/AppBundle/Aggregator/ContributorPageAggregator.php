8e58f13691eba15553fe6f25aa62ea84ca483832
Andrey Astakhov <andrey.astakhov@gmail.com>
2017-02-10 10:53:41 +0100

    Implemented aggregator for contributor logins.

--- src/AppBundle/Aggregator/ContributorPageAggregator.php
+++ src/AppBundle/Aggregator/ContributorPageAggregator.php
@@ -2,24 +2,18 @@
 
 namespace AppBundle\Aggregator;
 
-use AppBundle\Aggregator\Helper\CrawlerExtractorInterface;
-use AppBundle\Client\PageCrawler\PageCrawlerInterface;
+use AppBundle\Api\ContributorPage\ContributorPageApiInterface;
 use AppBundle\Entity\Project;
+use AppBundle\Entity\SensiolabsUser;
 use AppBundle\Helper\ProgressInterface;
 use AppBundle\Repository\SensiolabsUserRepository;
-use AppBundle\Util\StringUtils;
 
 class ContributorPageAggregator implements AggregatorInterface
 {
     /**
-     * @var CrawlerExtractorInterface
+     * @var ContributorPageApiInterface
      */
-    private $extractor;
-
-    /**
-     * @var PageCrawlerInterface
-     */
-    private $pageCrawler;
+    private $pageApi;
 
     /**
      * @var SensiolabsUserRepository
@@ -34,21 +28,18 @@ class ContributorPageAggregator implements AggregatorInterface
     /**
      * Constructor.
      *
-     * @param PageCrawlerInterface $pageCrawler
-     * @param CrawlerExtractorInterface $extractor
+     * @param ContributorPageApiInterface $pageApi
      * @param SensiolabsUserRepository $repository
      * @param string $profileUri
      */
     public function __construct(
-        PageCrawlerInterface $pageCrawler,
-        CrawlerExtractorInterface $extractor,
+        ContributorPageApiInterface $pageApi,
         SensiolabsUserRepository $repository,
         $profileUri
     ) {
-        $this->pageCrawler = $pageCrawler;
-        $this->extractor = $extractor;
         $this->repository = $repository;
         $this->profileUri = $profileUri;
+        $this->pageApi = $pageApi;
     }
 
     /**
@@ -56,33 +47,28 @@ class ContributorPageAggregator implements AggregatorInterface
      */
     public function aggregate(Project $project, array $options, ProgressInterface $progress = null)
     {
-        // @todo: get link from db
-        $url = 'http://symfony.com/contributors/code';
+        $logins = $this->pageApi->getContributorLogins($project->getContributorPageUri(), $this->profileUri);
+        $existingLogins = $this->repository->getExistingLogins($logins);
+        $missingLogins = array_diff($logins, $existingLogins);
 
-        $links = $this->getContributorLinks($url);
+        foreach ($missingLogins as $login) {
 
-        // @todo: iterate links, extract login, save to db
+            $user = new SensiolabsUser();
+            $user
+                ->setLogin($login)
+                ->setName($login)
+                ->setContributorId(0)
+                ->setCreatedAt(new \DateTime())
+                ->setUpdatedAt(new \DateTime())
+            ;
+
+            $this->repository->persist($user);
+        }
+
+        $this->repository->flush();
 
         return null;
     }
 
-    private function getContributorLinks($uri)
-    {
-        $crawler = $this->pageCrawler->getDomCrawler($uri);
-
-        $links = $this->extractor->extract($crawler);
-
-        return $links;
-    }
-
-    /**
-     * @param string $uri
-     *
-     * @return string
-     */
-    private function getSensiolabsLoginFromUri($uri)
-    {
-        return StringUtils::textAfter($uri, $this->profileUri);
-    }
 
 }
