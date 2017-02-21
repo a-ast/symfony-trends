ab7acf0bb95092d333c0e3ed34d0615fdd9b3673
Andrey Astakhov <andrey.astakhov@gmail.com>
2017-02-21 16:40:02 +0100

    Implemented basic ReportDumper with only 1 report message.

---
+++ spec/AppBundle/Aggregator/Report/ReportDumperSpec.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace spec\AppBundle\Aggregator\Report;
+
+use AppBundle\Aggregator\Report\ReportDumper;
+use Aa\ATrends\Aggregator\Report\ReportInterface;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\Console\Output\NullOutput;
+
+/**
+ * @mixin ReportDumper
+ */
+class ReportDumperSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(ReportDumper::class);
+    }
+
+    public function it_dumps_report(ReportInterface $report)
+    {
+        $this->dump($report, new NullOutput());
+
+        $report->getProcessedRecordCount()->shouldHaveBeenCalled();
+    }
+}
---
+++ src/AppBundle/Aggregator/Report/ReportDumper.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Aggregator\Report;
+
+use Aa\ATrends\Aggregator\Report\ReportInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ReportDumper
+{
+    public function dump(ReportInterface $report, OutputInterface $output)
+    {
+        $table = new Table($output);
+
+        $table
+            ->addRow(['Records processed', $report->getProcessedRecordCount()])
+            ->render();
+    }
+}
