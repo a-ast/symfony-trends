ca43d400f41e0afb21de8d41f0b6a78d9b4b878b
aa <andrey.astakhov@gmail.com>
2017-03-28 23:14:06 +0200

    Implemented PullRequestReview with all named constructors.

--- src/ATrends/spec/Api/Github/Model/PullRequestReviewSpec.php
+++ src/ATrends/spec/Api/Github/Model/PullRequestReviewSpec.php
@@ -6,10 +6,39 @@ use Aa\ATrends\Api\Github\Model\PullRequestReview;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
+/**
+ * @mixin PullRequestReview
+ */
 class PullRequestReviewSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
+        $this->beConstructedThrough('createFromArray', [$this->getArrayData()]);
         $this->shouldHaveType(PullRequestReview::class);
     }
+
+    function it_can_be_created_from_response_data()
+    {
+        $this->beConstructedThrough('createFromResponseData', [$this->getResponseData()]);
+        $this->shouldHaveType(PullRequestReview::class);
+    }
+
+    private function getArrayData()
+    {
+        return [
+            'id' => 100,
+            'state' => 'reviewed',
+            'userId' => 111,
+        ];
+    }
+
+    private function getResponseData()
+    {
+        return [
+            'id' => 100,
+            'state' => 'reviewed ',
+            'user' => ['id' => 111],
+        ];
+    }
+
 }
--- src/ATrends/src/Api/Github/Model/PullRequestReview.php
+++ src/ATrends/src/Api/Github/Model/PullRequestReview.php
@@ -20,6 +20,45 @@ class PullRequestReview
     private $userId;
 
     /**
+     * Constructor.
+     */
+    private function __construct()
+    {
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return PullRequestReview
+     */
+    public static function createFromArray(array $data)
+    {
+        $pullRequest = new PullRequestReview();
+
+        $pullRequest->id = (int)$data['id'];
+        $pullRequest->state = $data['state'];
+        $pullRequest->userId = (int)$data['userId'];
+
+        return $pullRequest;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @return PullRequestReview
+     */
+    public static function createFromResponseData(array $data)
+    {
+        $pullRequest = new PullRequestReview();
+
+        $pullRequest->id = (int)$data['id'];
+        $pullRequest->state = $data['state'];
+        $pullRequest->userId = (int)$data['user']['id'];
+
+        return $pullRequest;
+    }
+
+    /**
      * @return int
      */
     public function getId()
