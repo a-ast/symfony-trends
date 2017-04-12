ca04a7a248e3a60ad38877481741ed5f197f6090
aa <andrey.astakhov@gmail.com>
2017-04-12 23:21:36 +0200

    Added method to remove  existing reviews.

--- src/ATrends/src/Repository/PullRequestReviewRepository.php
+++ src/ATrends/src/Repository/PullRequestReviewRepository.php
@@ -7,4 +7,13 @@ namespace Aa\ATrends\Repository;
  */
 class PullRequestReviewRepository extends Repository
 {
+    public function removeByPullRequestId($id)
+    {
+        $this->createQueryBuilder('r')
+            ->delete()
+            ->where('r.pullRequestId = :id')
+            ->setParameter('id', $id)
+            ->getQuery()
+            ->execute();
+    }
 }
